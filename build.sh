4afae92886e873bf1487e1e7ea4ec32159bf7399
ManjilP <pantamanjil85@gmail.com>
2026-03-15 12:03:39 +0545

    Update build.sh with auto-superuser creation

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "from accounts.models import User; User.objects.filter(email='admin@futslab.com').exists() or User.objects.create_superuser('admin@futslab.com', 'Admin', 'Admin@123')"
